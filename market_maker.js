aac0dcb90d50e79226022357fd521382b5aaf6e8
sat hicham <adwikelove@gmail.com>
2017-08-14 23:45:33 +0000

    Update market_maker.js

--- market_maker.js
+++ market_maker.js
@@ -15,7 +15,7 @@ if (cliOptions.help) {
   API.init(() => {
     API.logs(() => {
       const pairs = marketMakerConfig.pairs;
-      API.getEtherDeltaTokenBalances(marketMakerConfig.account.address, (errBalances, balances) => {
+      API.getEtheRooxTokenBalances(marketMakerConfig.account.address, (errBalances, balances) => {
         API.getOrdersRemote((err, result) => {
           if (!err) {
             async.eachSeries(
