42e18afbe8478e30130e892870301f98eb6bec9e
sat hicham <adwikelove@gmail.com>
2017-08-14 23:00:49 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ $(() => {
   $('body').on('click', '#accountSubmit', (e) => {
     e.preventDefault();
     $('#accountModal').modal('hide');
-    bundle.EtherDelta.addAccount($('#accountAddr').val(), $('#accountPk').val());
+    bundle.EtheRoox.addAccount($('#accountAddr').val(), $('#accountPk').val());
   });
 });
 function buyChange() { // eslint-disable-line no-unused-vars
   const amount = Number($('#buyAmount').val());
   const price = Number($('#buyPrice').val());
   const total = (amount * price).toFixed(3);
-  const isInCross = bundle.EtherDelta.isInCross(price, 'buy');
+  const isInCross = bundle.EtheRoox.isInCross(price, 'buy');
   if (isInCross) {
     $('#buyInCross').html(`Your order is in cross with the best sell order in the order book (price = ${isInCross}).`);
   } else {
@@ -24,7 +24,7 @@ function sellChange() { // eslint-disable-line no-unused-vars
   const amount = Number($('#sellAmount').val());
   const price = Number($('#sellPrice').val());
   const total = (amount * price).toFixed(3);
-  const isInCross = bundle.EtherDelta.isInCross(price, 'sell');
+  const isInCross = bundle.EtheRoox.isInCross(price, 'sell');
   if (isInCross) {
     $('#sellInCross').html(`Your order is in cross with the best buy order in the order book (price = ${isInCross}).`);
   } else {
@@ -35,7 +35,7 @@ function sellChange() { // eslint-disable-line no-unused-vars
 $(() => {
   $('body').on('click', '#buySubmit', (e) => {
     e.preventDefault();
-    bundle.EtherDelta.order(
+    bundle.EtheRoox.order(
       'buy',
       $('#buyAmount').val(),
       $('#buyPrice').val(),
@@ -46,7 +46,7 @@ $(() => {
 $(() => {
   $('body').on('click', '#sellSubmit', (e) => {
     e.preventDefault();
-    bundle.EtherDelta.order(
+    bundle.EtheRoox.order(
       'sell',
       $('#sellAmount').val(),
       $('#sellPrice').val(),
@@ -104,14 +104,14 @@ $(() => {
   $('body').on('click', '#buyCrossSubmit', (e) => {
     e.preventDefault();
     $('#buyCrossModal').modal('hide');
-    bundle.EtherDelta.trade('buy', JSON.parse($('#buyCrossOrder').val()), $('#buyCrossAmount').val());
+    bundle.EtheRoox.trade('buy', JSON.parse($('#buyCrossOrder').val()), $('#buyCrossAmount').val());
   });
 });
 $(() => {
   $('body').on('click', '#sellCrossSubmit', (e) => {
     e.preventDefault();
     $('#sellCrossModal').modal('hide');
-    bundle.EtherDelta.trade(
+    bundle.EtheRoox.trade(
       'sell',
       JSON.parse($('#sellCrossOrder').val()),
       $('#sellCrossAmount').val());
@@ -121,7 +121,7 @@ $(() => {
   $('body').on('click', '#otherTokenSubmit', (e) => {
     e.preventDefault();
     $('#otherTokenModal').modal('hide');
-    bundle.EtherDelta.selectToken(
+    bundle.EtheRoox.selectToken(
       $('#otherTokenAddr').val(),
       $('#otherTokenName').val(),
       $('#otherTokenDecimals').val());
@@ -131,30 +131,30 @@ $(() => {
   $('body').on('click', '#otherBaseSubmit', (e) => {
     e.preventDefault();
     $('#otherBaseModal').modal('hide');
-    bundle.EtherDelta.selectBase(
+    bundle.EtheRoox.selectBase(
       $('#otherBaseAddr').val(),
       $('#otherBaseName').val(),
       $('#otherBaseDecimals').val());
   });
 });
 $('#gasPriceModal').on('show.bs.modal', () => {
-  $('#gasPrice').val(bundle.EtherDelta.config.ethGasPrice / 1000000000);
+  $('#gasPrice').val(bundle.EtheRoox.config.ethGasPrice / 1000000000);
 });
 $(() => {
   $('body').on('click', '#gasPriceSubmit', (e) => {
     e.preventDefault();
     $('#gasPriceModal').modal('hide');
-    bundle.EtherDelta.setGasPrice(
+    bundle.EtheRoox.setGasPrice(
       $('#gasPrice').val());
   });
 });
 function depositClick(addr) { // eslint-disable-line no-unused-vars
-  bundle.EtherDelta.deposit(addr, $(`#depositAmount${addr}`).val());
+  bundle.EtheRoox.deposit(addr, $(`#depositAmount${addr}`).val());
 }
 function withdrawClick(addr) { // eslint-disable-line no-unused-vars
-  bundle.EtherDelta.withdraw(addr, $(`#withdrawAmount${addr}`).val());
+  bundle.EtheRoox.withdraw(addr, $(`#withdrawAmount${addr}`).val());
 }
 function transferClick(addr) { // eslint-disable-line no-unused-vars
-  bundle.EtherDelta.transfer(addr, $(`#transferAmount${addr}`).val(), $(`#transferTo${addr}`).val());
+  bundle.EtheRoox.transfer(addr, $(`#transferAmount${addr}`).val(), $(`#transferTo${addr}`).val());
 }
 $(() => {});
