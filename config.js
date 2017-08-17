1137771ba1171db6490204582f60e3fb38136777
sat hicham <adwikelove@gmail.com>
2017-08-17 00:15:46 +0000

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ module.exports = {
   gasWithdraw: 250000,
   gasTrade: 250000,
   gasOrder: 250000,
-  ordersOnchain: false,
+  ordersOnchain: true,
   apiServer: 'http://etheroox.com',
   userCookie: 'EtheRoox',
   eventsCacheCookie: 'EtheRoox_eventsCache',
